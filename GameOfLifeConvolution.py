cb47f51b61a33c5beb6de14c691edf6d84bbe796
Francisco Angelo <franciscoangelo.feo@gmail.com>
2021-09-11 11:58:58 -0300

    Put gifs on README.md

--- GameOfLifeConvolution.py
+++ GameOfLifeConvolution.py
@@ -4,12 +4,11 @@ import imageio
 import os
 from scipy import signal
 
-# Tamanho da matriz (N x N)
+# Matrix size (N x N)
 N = 50;
 
-presente = np.random.randint(0, 2, [N, N]);
-#presente = np.zeros(N*N).reshape(N, N)
-
+present = np.random.randint(0, 2, [N, N]);
+#present = np.zeros(N*N).reshape(N, N)
 
 def block(matrix, x, y):
     matrix[x: x+2, y: y+2] = np.array([[1, 1], [1, 1]]);
@@ -21,31 +20,32 @@ def spaceship(matrix, x, y):
     matrix[x: x+4, y: y+6] = np.array([[0,0,0,1,1,0], [1,1,1,0,1,1], [1,1,1,1,1,0], [0,1,1,1,0,0]])
 
 kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]]);
+#spaceship(present, 4, 4)
 
 files = [];
 
-for n in range(0, 100):
+for n in range(0, 180):
 
     if n == 0:
-        plt.imshow(presente, cmap="binary");
+        plt.imshow(present, cmap="binary");
         plt.title(f"Game of Life");
         #plt.show()
         filename1 = f"{n}.png";
         files.append(filename1);
         plt.savefig(filename1);
 
-    mapa_convolucionado = signal.convolve2d(presente, kernel, mode="same", boundary="wrap" );
-    presente = (((presente == 1) & (mapa_convolucionado > 1) & (mapa_convolucionado < 4)) | ((presente == 0) & (mapa_convolucionado == 3)));
+    convol_world = signal.convolve2d(present, kernel, mode="same", boundary="wrap" );
+    present = (((present == 1) & (convol_world > 1) & (convol_world < 4)) | ((present == 0) & (convol_world == 3)));
     
-    plt.imshow(presente, cmap="binary");
-    plt.title(f"Game of Life - Geração {n}");
+    plt.imshow(present, cmap="binary");
+    plt.title(f"Game of Life - Generation {n}");
     #plt.show()
     filename = f"{n}.png";
     files.append(filename);
     plt.savefig(filename);
 
 
-with imageio.get_writer('pretobranco3.gif', mode='I') as writer:
+with imageio.get_writer('myGif.gif', mode='I') as writer:
     for filename in files:
         image = imageio.imread(filename);
         writer.append_data(image);
--- README.md
+++ README.md
@@ -1,11 +1,21 @@
+
 # John Conway's Game of Life :fire:
 
-Using theory of convolutions we can optimize this script and decrease your compilation time. I'm using [Numpy](https://numpy.org/) to create the matrices and [Scipy](https://www.scipy.org/) to realize the convolution. Therefore, you need to install these packages in your machine.
+Using theory of convolutions we can optimize this script and decrease your compilation time. I'm using [Numpy](https://numpy.org/) to create the matrices and [Scipy](https://www.scipy.org/) to realize the convolution and [Matplotlib](https://matplotlib.org
+) to plot the generations. Therefore, you need to install these packages in your machine.
 
-*Run this in your command line to install numpy:
-> pip install numpy
+- Run this in your command line to install numpy:
 
-*And this to install scipy:
-> pip install scipy
+    ``` pip install numpy ```
 
-Now you are ready to enjoy the Game of Life! :zap:
+- Run this to install scipy:
+
+    ``` pip install scipy ```
+
+- And this to install matplotlib:
+  
+  ```pip install matplotlib ```
+
+Now you are ready to enjoy the Game of Life! :zap: 
+
+<img src="myGif.gif" alt="spaceship" style="width:400px;"/>
