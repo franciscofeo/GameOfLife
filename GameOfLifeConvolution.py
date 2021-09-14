911a0d027f4a6e84176e7349d0e84b88428fb5be
Francisco Angelo <franciscoangelo.feo@gmail.com>
2021-09-14 14:51:45 -0300

    Add new patterns and remove the conditionals

--- GameOfLifeConvolution.py
+++ GameOfLifeConvolution.py
@@ -5,7 +5,7 @@ import os
 from scipy import signal
 
 # Matrix size (N x N)
-N = 50;
+N = 25;
 
 #present = np.random.randint(0, 2, [N, N]); # random N x N matrix with 0 or 1
 present = np.zeros(N*N).reshape(N, N) # N x N matrix with all elements = 0
@@ -23,20 +23,23 @@ def toad(matrix, x, y):
     matrix[x: x+2, y: y+4] = np.array([[0,1,1,1], [1,1,1,0]]);
 
 def dieHard(matrix, x, y):
-    matrix[x: x+3, y: y+8] = np.array([[0,0,0,0,0,0,1,0], [1,1,0,0,0,0,0,0], [0,1,0,0,0,1,1,1]])
+    matrix[x: x+3, y: y+8] = np.array([[0,0,0,0,0,0,1,0], [1,1,0,0,0,0,0,0], [0,1,0,0,0,1,1,1]]);
+
+def dora_cell(matrix, x, y):
+    matrix[x: x+2, y: y+5] = np.array([[0, 0, 1, 1, 1], [1, 1, 1, 0, 0]])
 
 kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]]);
-spaceship(present, 3,3)
+dieHard(present, 10, 8)
 
 files = [];
 
 plt.imshow(present, cmap="binary");
-plt.title(f"Game of Life");
+plt.title(f"Game of Life - Generation 0");
 filename1 = f"{0}.png";
 files.append(filename1);
 plt.savefig(filename1);
 
-for n in range(1, 101):
+for n in range(1, 131):
 
     convol_world = signal.convolve2d(present, kernel, mode="same", boundary="wrap" );
     present = (((present == 1) & (convol_world > 1) & (convol_world < 4)) | ((present == 0) & (convol_world == 3)));
@@ -48,7 +51,7 @@ for n in range(1, 101):
     plt.savefig(filename);
 
 
-with imageio.get_writer('myGif.gif', mode='I') as writer:
+with imageio.get_writer('dieHard.gif', mode='I') as writer:
     for filename in files:
         image = imageio.imread(filename);
         writer.append_data(image);
