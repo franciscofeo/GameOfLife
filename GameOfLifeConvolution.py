d07da0821de4e832af4c212058eec06247197325
Francisco Angelo <franciscoangelo.feo@gmail.com>
2021-09-16 13:01:20 -0300

    Add gosper's glider gun and LICENSE.md

--- GameOfLifeConvolution.py
+++ GameOfLifeConvolution.py
@@ -5,7 +5,7 @@ import os
 from scipy import signal
 
 # Matrix size (N x N)
-N = 25;
+N = 50;
 
 #present = np.random.randint(0, 2, [N, N]); # random N x N matrix with 0 or 1
 present = np.zeros(N*N).reshape(N, N) # N x N matrix with all elements = 0
@@ -26,10 +26,16 @@ def dieHard(matrix, x, y):
     matrix[x: x+3, y: y+8] = np.array([[0,0,0,0,0,0,1,0], [1,1,0,0,0,0,0,0], [0,1,0,0,0,1,1,1]]);
 
 def dora_cell(matrix, x, y):
-    matrix[x: x+2, y: y+5] = np.array([[0, 0, 1, 1, 1], [1, 1, 1, 0, 0]])
+    matrix[x: x+2, y: y+5] = np.array([[0, 0, 1, 1, 1], [1, 1, 1, 0, 0]]);
+
+def glider_gun(matrix, x, y):
+    matrix[x: x+9, y: y+36] = np.array([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1], [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1], [1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]])
 
 kernel = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]]);
-dieHard(present, 10, 8)
+glider_gun(present, 10, 8)
 
 files = [];
 
@@ -39,7 +45,7 @@ filename1 = f"{0}.png";
 files.append(filename1);
 plt.savefig(filename1);
 
-for n in range(1, 131):
+for n in range(1, 180):
 
     convol_world = signal.convolve2d(present, kernel, mode="same", boundary="wrap" );
     present = (((present == 1) & (convol_world > 1) & (convol_world < 4)) | ((present == 0) & (convol_world == 3)));
